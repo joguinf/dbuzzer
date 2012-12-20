ba0b344005b1b64aaa274c8f2191272968302f5a
Christian Hundt <hundt.christian@googlemail.com>
2012-12-20 16:04:09 +0100

    implemented everything but the buzzer feature

--- server.py
+++ server.py
@@ -5,21 +5,74 @@ import shlex
 class EchoHandler(asyncore.dispatcher_with_send):
 
     def __init__(self, ident, *args, **kwargs):
-        
+
+        # call superclass init
         asyncore.dispatcher_with_send.__init__(self, *args, **kwargs)
 
+        # set ident and name
         self.ident = ident
-        self.send("your id is %s\n" % str(ident))
-        
+        self.nick  = str(ident)
 
     def handle_read(self):
+    
         data = self.recv(8192)
+
+        methods = {"help": self.print_help,
+                   "nick": self.set_nick,
+                   "list": self.list_players,
+                   "say" : self.say}
+
         if data:
-            command = shlex.split(data)
-            if command[0] not in ["help", "buzzer", "say"]:
-                self.send("command %s not found\n" % command[0])
+        
+            message = shlex.split(data)
+        
+            if message[0] not in methods:
+                self.send("command %s not found\n\n" % message[0])
             else:
-                self.send(data)
+                methods[message[0]](message)
+              
+    def print_help(self, *args, **kwds):
+
+        message = r"""
+USAGE:
+
+    help ...................... print out help page
+    list ...................... list players
+    nick ...................... show your own nick 
+    nick Larry ................ set your nick to "Larry"
+    buzzer .................... press the buzzer button
+    say Hello ................. say "Hello" to the people
+    
+"""
+        self.send(message)
+
+    def set_nick(self, *args, **kwds):
+    
+        if len(args[0]) == 1:
+        
+            if str(self.ident) == self.nick:
+                self.send('first set your nickname with "nick nickname"\n\n')
+            else:
+                self.send("your nick is: %s\n\n" % self.nick)
+        else:
+            self.nick="%s(%s)" % (" ".join(args[0][1:]), self.ident[1])
+            self.send('your nick was changed to: %s\n\n' % self.nick)
+
+    def list_players(self, *args, **kwds):
+
+        self.send(", ".join(server.get_clients())+"\n\n")
+
+    def say(self, *args, **kwds):
+
+        if len(args[0]) > 1:
+        
+            if str(self.ident) == self.nick:
+                self.send('first set your nickname with "nick nickname"\n\n')
+            else:
+                message = " ".join(args[0][1:])
+                server.broadcast("%s: %s\n" % (self.nick, message))
+        else:
+            self.send("(said nothing)\n\n")
 
 class EchoServer(asyncore.dispatcher):
 
@@ -29,6 +82,31 @@ class EchoServer(asyncore.dispatcher):
         self.set_reuse_addr()
         self.bind((host, port))
         self.listen(5)
+        
+        self.connections = []
+
+    def broadcast(self, text, newline=True):
+
+        for client in self.connections:
+            try:
+                client.send(text+"\n" if newline else "")
+            except:
+                pass
+
+    def get_clients(self):
+    
+        clients = []
+        
+        for client in self.connections:
+            try:
+                if client.nick == str(client.ident):
+                    clients.append("no_nick_set!(%s)" % client.ident[1])
+                else:
+                    clients.append(client.nick)
+            except:
+                pass
+        
+        return clients
 
     def handle_accept(self):
         pair = self.accept()
@@ -42,9 +120,10 @@ class EchoServer(asyncore.dispatcher):
 Welcome to the Quizshow!
 ========================================  
 
-___________________________________ 
-< Send "help" for more information! >
- ----------------------------------- 
+ _____________________________________ 
+/ Set your nick with "nick Larry" and \
+\ list the help page with "help"      /
+ ------------------------------------- 
         \   ^__^
          \  (oo)\_______
             (__)\       )\/\
@@ -54,7 +133,7 @@ ___________________________________
 """
             sock.send(greeting)
             handler = EchoHandler(addr, sock)
-
+            self.connections.append(handler)
 
 server = EchoServer('localhost', 8080)
 asyncore.loop()
